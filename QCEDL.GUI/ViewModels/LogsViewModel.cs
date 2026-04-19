b5fe07ae9bf445bc4bad4fb2ff4c59ecf32b9a83
Littlenine <LittlenineEnnea@gmail.com>
2026-04-20 01:45:59 +0800

    Add Copy Function for Log Pages
    Signed-off-by: Littlenine <LittlenineEnnea@gmail.com>

--- QCEDL.GUI/ViewModels/LogsViewModel.cs
+++ QCEDL.GUI/ViewModels/LogsViewModel.cs
@@ -1,19 +1,43 @@
+using System.Collections.Specialized;
 using QCEDL.GUI.Services;
+using ReactiveUI;
 
 namespace QCEDL.GUI.ViewModels;
 
 public sealed class LogsViewModel : ViewModelBase
 {
+    private string _text = string.Empty;
+
     public LogsViewModel(ObservableLogSink sink)
     {
         ArgumentNullException.ThrowIfNull(sink);
         Sink = sink;
+        Sink.Entries.CollectionChanged += OnEntriesChanged;
+        UpdateText();
     }
 
     public ObservableLogSink Sink { get; }
 
+    public string Text
+    {
+        get => _text;
+        private set => this.RaiseAndSetIfChanged(ref _text, value);
+    }
+
     public void Clear()
     {
         Sink.Clear();
+        UpdateText();
+    }
+
+    private void OnEntriesChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        // Format each log entry as "Time Level Message" and join them with new lines
+        Text = string.Join(Environment.NewLine, Sink.Entries.Select(entry => $"{entry.TimeText} {entry.LevelText} {entry.Message}"));
     }
 }
